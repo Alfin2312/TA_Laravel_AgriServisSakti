7014e1c9fd72650158e66e7e2f1e7607911425e7
alfin2312 <alfin.mulya1999@gmail.com>
2021-06-04 04:40:32 +0700

    fix frontend admin, login dan register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function getLogin()
+    {
+        return view('auth.v_login1');
+    }
+
+    public function postLogin(Request $request)
+    {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->back();
+        } else {
+            if (Auth::attempt(['role' => 2, 'password' => $request->password])) {
+                return redirect('pt_delaval');
+            } else {
+                return redirect('/');
+            }
+        }
+
+        return redirect('/');
+    }
+
+    public function getRegister()
+    {
+        return view('auth.v_register1');
+    }
+
+    public function postRegister(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed', //field_confirmation
+
+        ]);
+
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'role' => '2',
+                'password' => bcrypt($request->password)
+            ]);
+
+            return redirect()->route('login')->with('pesan', 'Register Berhasil !!');
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
